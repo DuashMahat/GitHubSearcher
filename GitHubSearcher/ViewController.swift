56e1288825606fb55445af782002794258ade527
Duale Abdullahi <abdull53@msu.edu>
2020-06-23 15:20:00 -0500

    Initial Commit

---
+++ GitHubSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearcher
+//
+//  Created by Duale on 6/23/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
